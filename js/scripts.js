cbef407e5b60464a2c71b12e292a2c2d046086ef
Kate Farkas <kate.m.farkas@gmail.com>
2020-03-30 10:24:28 -0700

    collect answers and console.log in array

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  
+  $("#favorite-form").submit(function(event) {
+    // var catAnswers;
+    // $("input#cat-type").val();
+    // $("input#cat-part").val();
+    // $("input#cat-color").val();
+    
+var catAnswers = [$("input#cat-type").val(),
+    $("input#cat-part").val(),
+    $("input#cat-color").val()];
+
+    console.log(catAnswers);
+  
+    event.preventDefault();
+  })
+})
