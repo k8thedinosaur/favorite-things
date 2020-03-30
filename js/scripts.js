64e81c46a6e96f0ab3bd910f17011b059561e08b
Kate Farkas <kate.m.farkas@gmail.com>
2020-03-30 10:45:28 -0700

    add js to target array items and display in span; styling

--- js/scripts.js
+++ js/scripts.js
@@ -6,13 +6,17 @@ $(document).ready(function() {
     // $("input#cat-part").val();
     // $("input#cat-color").val();
     
-var catAnswers = [$("input#cat-type").val(),
+    var catAnswers = [$("input#cat-type").val(),
     $("input#cat-part").val(),
     $("input#cat-color").val()];
     console.log(catAnswers);
 
+    $(".cat-type").append(catAnswers[0]);
+    $(".cat-part").append(catAnswers[1]);
+    $(".cat-color").append(catAnswers[2]);
+
+    $("#answers").show();
     
-  
     event.preventDefault();
   })
 })
